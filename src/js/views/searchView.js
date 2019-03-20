d057918cc7018efa9772c5d8707834be335487b2
Prune2000 <camille@plommetre.com>
2019-03-20 21:26:38 +0100

    Filter events by date and only show the ones that are coming up

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -25,24 +25,24 @@ const renderCoinInfo = coin => {
 const renderTweet = tweet => {
     const markup = `
         <div class="tweet_card">
-            <img src="${tweet.user_image_link}" alt="${tweet.user_name}" class="profile-thumbnail">
-        <div class="profile-name">
-            <h4>@${tweet.user_name}</h4>
-        </div>
-        <div id="inner">
-            <p>${tweet.status}</p>
-            <span class="date">${tweet.date}</span>
-            <hr>
-        </div>
-        <div class="stats">
-            <div class="retweets">
-            <strong>${tweet.retweet_count}</strong> Retweets
+                <img src="${tweet.user_image_link}" alt="${tweet.user_name}" class="profile-thumbnail">
+            <div class="profile-name">
+                <h4>@${tweet.user_name}</h4>
             </div>
-            <div class="likes">
-            <strong>${tweet.like_count}</strong> Likes
+            <div id="inner">
+                <p>${tweet.status}</p>
+                <span class="date">${tweet.date}</span>
+                <hr>
+            </div>
+            <div class="stats">
+                <div class="retweets">
+                <strong>${tweet.retweet_count}</strong> Retweets
+                </div>
+                <div class="likes">
+                <strong>${tweet.like_count}</strong> Likes
+                </div>
             </div>
         </div>
-    </div>
     `;
     elements.twitterSection.insertAdjacentHTML('beforeend', markup);
 };
@@ -50,17 +50,16 @@ const renderTweet = tweet => {
 const renderEvent = event => {
     const markup = `
         <div class="event_card">
-            <img src="${event.proof_image_link}" alt="${event.id}" class="event-thumbnail">
-        <div class="event-name">
-            <h4>${event.name}</h4>
+            <div class="event-name">
+                <h4>${event.name}</h4>
+            </div>
+            <div id="inner_event">
+                <p>${event.description}</p>
+                <a href="${event.link}"><p>Link to the event information</p></a>
+                <span class="date">${event.date}</span>
+                <hr>
+            </div>
         </div>
-        <div id="inner_event">
-            <p>${event.description}</p>
-            <a href="${event.link}"><p>Link to the event information</p></a>
-            <span class="date">${event.date}</span>
-            <hr>
-        </div>
-    </div>
     `;
     elements.eventSection.insertAdjacentHTML('beforeend', markup);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, 'src'),
-        filename: '.src/js/bundle.js'
+        filename: './js/bundle.js'
     },
     devServer: {
         contentBase: './src',
